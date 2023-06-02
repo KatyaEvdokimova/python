3351dc250d9a3bc1809d85fdb7da50102a0e8517
First site <katenn11@mail.ru>
2023-06-02 19:01:31 +0300

    Commit number one

---
+++ main.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def sum(a,c):
+        return a + c
+
+if __name__ == '__main__':
+        print(sum(4,5))
---
+++ test_main.py
@@ -0,0 +1,5 @@
+import pytest
+from main import sum
+
+def hello(a,b):
+   print(sum(5,5))
